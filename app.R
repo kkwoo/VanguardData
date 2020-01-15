030100327905dd84faa43bfd7f7f73676818e3c3
kkwoo <kkwoo@users.noreply.github.com>
2020-01-15 20:09:29 +1000

    Dynamically load investment options

--- app.R
+++ app.R
@@ -20,10 +20,23 @@ vanguardData <- function (productIDStr, startDate, endDate) {
                      ",issueType:S,sDate:",startDate,
                      ",eDate:",endDate,
                      ",as_of:DAILY&callback=angular.callbacks._4")
-    "https://api.vanguard.com/rs/gre/gra/1.7.0/datasets/auw-retail-price-history-mf.jsonp?vars=portId:8145,issueType:S,sDate:2018-02-28,eDate:2019-09-28,as_of:DAILY"
+    # "https://api.vanguard.com/rs/gre/gra/1.7.0/datasets/auw-retail-price-history-mf.jsonp?vars=portId:8145,issueType:S,sDate:2018-02-28,eDate:2019-09-28,as_of:DAILY"
     rawData <- GET(urlStr) 
     return (fromJSON(strsplit(content(rawData, "text"), "[()]")[[1]][[2]]))
 }
+
+vanguardProducts <- function () {
+    urlStr <- paste0("https://api.vanguard.com/rs/gre/gra/1.7.0/datasets/auw-retail-listview-data.jsonp?callback=angular.callbacks._0")
+    rawData <- GET(urlStr) 
+    sr01 <- stri_replace_first_fixed(content(rawData, "text"), "angular.callbacks._0(", "")
+    sr02 <- stri_replace_last_fixed(sr01, ")", "")
+    return (fromJSON(sr02)$fundData)
+}
+x16 <- Filter(function(x) !is.null(x$name), vanguardProducts())
+x17 <- unlist(Map(function(x) paste(x$portId, "-", x$name), x16))
+x18 <- Map(function (x) x$portId, x16)
+x19 <- setNames(x18, x17)
+x19 <- x19[order(names(x19))]
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -36,9 +49,11 @@ ui <- fluidPage(
         sidebarPanel(
             dateInput("date1", "Date:", value = as.Date("01/02/2014", format = "%d/%m/%Y")),
             dateInput("date2", "Date:", value = Sys.Date()),
-            selectInput("productCode", "Product Code", c("8145 - International Shares" = "8145",
-                                                         "8146 - Hedged International Shares" = "8146",
-                                                         "8129 - Australian Shares Fund" = "8129"))
+            # selectInput("productCode", "Product Code", c("8145 - International Shares" = "8145",
+            #                                             "8146 - Hedged International Shares" = "8146",
+            #                                             "8129 - Australian Shares Fund" = "8129",
+            #                                             "fixed" = "8149")) */
+            selectInput("productCode", "Product Code", x19)
         ),
         
         # Show a plot of the generated distribution
